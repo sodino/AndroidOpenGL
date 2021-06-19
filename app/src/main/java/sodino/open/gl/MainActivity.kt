08dd6cba7c20f2192a461ee755031db1dddec94c
sodino <sodino@qq.com>
2021-06-19 15:19:35 +0800

    [dev] copy wall.jpg from assets to '/data/user/0/sodino.open.gl/cache/wall.jpg', prepare for `stb_image_load`

--- app/src/main/java/sodino/open/gl/MainActivity.kt
+++ app/src/main/java/sodino/open/gl/MainActivity.kt
@@ -2,10 +2,21 @@ package sodino.open.gl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import sodino.open.gl.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity
+    : AppCompatActivity(),
+    CoroutineScope by MainScope(),
+    View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,6 +30,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.sampleText.text = "Hello World, myOpenGL."
         binding.btnDrawLineSwitch.setOnClickListener(this)
         setDrawLineText(false)
+
+        copyAsset2Storage()
+    }
+
+    private fun copyAsset2Storage() = launch(Dispatchers.IO) {
+        val fDest = File(cacheDir, "wall.jpg")
+        Log.d("appGL", "wall.jpg path=${fDest.absolutePath}")
+        if (fDest.exists()) {
+            return@launch
+        }
+        try {
+            val fParent = fDest.parentFile
+            if (!fParent.exists()) {
+                fDest.mkdirs()
+            }
+            if (fDest.exists()) {
+                fDest.delete()
+            }
+            fDest.createNewFile()
+
+            val inputStream = assets.open("wall.jpg")
+            val fos = FileOutputStream(fDest)
+
+            val buffer = ByteArray(1024 * 8)
+            var read = 0
+            while(true) {
+                read = inputStream.read(buffer)
+                if (read < 0) {
+                    break
+                }
+                fos.write(buffer, 0, read)
+                fos.flush()
+            }
+            Log.d("appGL", "write successfully, path=${fDest.absolutePath}")
+
+            inputStream.close()
+            fos.close()
+        } catch(e : IOException) {
+            fDest.delete()
+            e.printStackTrace()
+        }
     }
 
     private fun setDrawLineText(onlyLine : Boolean = false) {
